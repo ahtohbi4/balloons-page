86a75bb4f77ea3b556fc963210e29d3067d6fc52
Alexander Antonov <alexandr-post@yandex.ru>
2016-07-16 13:42:52 +0300

    gulpfile.js was updated

--- gulpfile.js
+++ gulpfile.js
@@ -1,22 +1,40 @@
 const gulp = require('gulp');
-
-const htmlmin = require('gulp-htmlmin');
 const rename = require('gulp-rename');
+const clean = require('gulp-clean');
+
+const posthtml = require('gulp-posthtml');
+const minifier = require('posthtml-minifier');
+
+const PATHS = {
+    src: './app/resources/views/pages/',
+    dest: './build/'
+};
+
+gulp.task('clean', () => {
+    gulp.src(PATHS.dest, {
+        read: false
+    })
+        .pipe(clean());
+});
 
 gulp.task('html', () => {
-    gulp.src('app/resources/views/pages/main.html')
+    gulp.src(`${PATHS.src}main.html`)
         .pipe(rename('index.html'))
-        .pipe(htmlmin({
-            collapseWhitespace: true,
-            removeComments: true
-        }))
-        .pipe(gulp.dest('./'));
+        .pipe(posthtml([
+            minifier({
+                collapseWhitespace: true,
+                removeComments: true
+            })
+        ]))
+        .pipe(gulp.dest(`${PATHS.dest}a/`))
+        .pipe(gulp.dest(`${PATHS.dest}b/`));
 });
 
 gulp.task('css', () => {
-    gulp.src('app/resources/views/pages/main.css')
+    gulp.src(`${PATHS.src}main.css`)
         .pipe(rename('style.css'))
-        .pipe(gulp.dest('./compiled/'));
+        .pipe(gulp.dest(`${PATHS.dest}a/`))
+        .pipe(gulp.dest(`${PATHS.dest}b/`));
 });
 
 gulp.task('default', [
@@ -25,6 +43,6 @@ gulp.task('default', [
 ]);
 
 gulp.task('watch', ['default'], () => {
-    gulp.watch(PATHS.src.css, ['css']);
-    gulp.watch(PATHS.src.html, ['html']);
+    gulp.watch(`${PATHS.src}*.css`, ['css']);
+    gulp.watch(`${PATHS.src}*.html`, ['html']);
 });
